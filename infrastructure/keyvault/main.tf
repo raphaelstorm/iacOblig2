022382e160c186db7a124addce8e803d8f21b3c0
raphaelstorm <raphaelstorm@users.noreply.github.com>
2023-11-01 19:15:55 +0000

    [AUTO]Format Terraform code
    Files changed:
    M	infrastructure/keyvault/main.tf

--- infrastructure/keyvault/main.tf
+++ infrastructure/keyvault/main.tf
@@ -25,13 +25,13 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_key_vault" "keyvault" {
-  name                          = format("%s-kv%s", var.prefix, var.suffix)
-  location                      = azurerm_resource_group.rg.location
-  resource_group_name           = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption   = true
-  tenant_id                     = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days    = 7
-  purge_protection_enabled      = true
+  name                        = format("%s-kv%s", var.prefix, var.suffix)
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = true
 
   sku_name = "standard"
 
