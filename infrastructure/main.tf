5679a16afa3f7bd2b3d32aa2916e3aa9ac3a32e6
raphaesl <raphaesl@stud.ntnu.no>
2023-11-02 21:19:40 +0100

    removed key vault 2

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,7 +16,8 @@ resource "random_password" "password" {
   override_special = "!%&*()-_=+[]{}<>:?"
 }
 
+/*
 output "vm_password" {
   value     = azurerm_key_vault_secret.vm_password.value
   sensitive = true
-}
+}*/
