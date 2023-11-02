b89a037e64f97a272365d9540cd9657b1f14801c
raphaesl <raphaesl@stud.ntnu.no>
2023-11-02 14:12:41 +0100

    new backend

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -15,7 +15,7 @@ terraform {
     resource_group_name  = "rsl-backend-rg"
     storage_account_name = "rslbackendsa"
     container_name       = "rsl-backend-sc"
-    key                  = "oblig2.terraform.tfstate"
+    key                  = "oblig.terraform.tfstate"
   }
 }
 
